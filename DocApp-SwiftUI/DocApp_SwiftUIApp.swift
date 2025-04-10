7658b8fa2a1c38a497366e6794c186632edc9ec9
majlindahaliti <majlinda@innosolutions.io>
2025-04-10 14:31:42 +0200

    Initial Commit

---
+++ DocApp-SwiftUI/DocApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocApp_SwiftUIApp.swift
+//  DocApp-SwiftUI
+//
+//  Created by Majlinda on 10.4.25.
+//
+
+import SwiftUI
+
+@main
+struct DocApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
