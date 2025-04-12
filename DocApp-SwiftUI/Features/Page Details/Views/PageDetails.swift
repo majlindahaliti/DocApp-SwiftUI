777a0dea4560ca7807123acae836b9d367c9f2ec
majlindahaliti <majlinda@innosolutions.io>
2025-04-12 21:34:28 +0200

    implement page details UI

--- DocApp-SwiftUI/Features/Page Details/Views/PageDetails.swift
+++ DocApp-SwiftUI/Features/Page Details/Views/PageDetails.swift
@@ -6,15 +6,56 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct PageDetails: View {
     var section: SectionItem
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 0) {
+                if let imgUrl = section.src{
+                    KFImage(URL(string: imgUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: UIScreen.main.bounds.width, height: 300)
+                        .roundedCorner(30, corners: [.bottomLeft, .bottomRight])
+                        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 2)
+                }
+                else{
+                    Image("coverImg")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: UIScreen.main.bounds.width, height: 300)
+                        .roundedCorner(30, corners: [.bottomLeft, .bottomRight])
+                        .clipped()
+                        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 2)
+                }
+                
+                Text(section.title)
+                    .font(Fonts.poppinsMedium(size: 20))
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    // Custom back action (if needed)
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.white)
+                        .imageScale(.large)
+                }
+            }
+        }
+        .ignoresSafeArea(edges: .top)
+        .background(Color.white)
     }
 }
 
 #Preview {
-    PageDetails(section: SectionItem(type: "page", title: "test"))
+    PageDetails(section: SectionItem(type: "page", title: "Test Title", items: [
+        SectionItem(type: "image", title: "Sample Image", src: "https://robohash.org/280?&set=set4&size=400x400")
+    ]))
 }
